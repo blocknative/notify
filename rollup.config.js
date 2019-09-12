0f333f6e58ef62b3a08a129cb86c6dd1c7c2d3da
Aaron Barnard <abarnard@protonmail.com>
2019-09-12 11:09:44 +1000

    updates

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import svelte from "rollup-plugin-svelte"
 import resolve from "rollup-plugin-node-resolve"
 import babel from "rollup-plugin-babel"
+import globals from "rollup-plugin-node-globals"
 import commonjs from "rollup-plugin-commonjs"
 import builtins from "rollup-plugin-node-builtins"
 import json from "rollup-plugin-json"
@@ -26,6 +27,7 @@ export default [
       commonjs(),
       babel({ exclude: "node_modules/**" }),
       builtins(),
+      globals(),
       terser()
     ]
   },
